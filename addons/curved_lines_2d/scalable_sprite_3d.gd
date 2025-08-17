8d9890e7c9261fec2c330918dd8e49a82ac33137
René van der Ark <r.van.der.ark@gmail.com>
2025-08-17 11:26:30 +0200

    a result I'm kind of happy with.

--- addons/curved_lines_2d/scalable_sprite_3d.gd
+++ addons/curved_lines_2d/scalable_sprite_3d.gd
@@ -12,22 +12,22 @@ var top_left_anchor := Node3D.new()
 var bottom_right_anchor := Node3D.new()
 var current_texture_idx := 0
 
-func _ready() -> void:
-	if is_instance_valid(texture_source):
-		var ptsd := texture_source.smallest_size * 0.5
-		top_left_anchor.position.x = -ptsd.x * pixel_size
-		top_left_anchor.position.y = -ptsd.y * pixel_size
-		bottom_right_anchor.position.x = ptsd.x * pixel_size
-		bottom_right_anchor.position.y = ptsd.y * pixel_size
+func _enter_tree() -> void:
+	if not top_left_anchor.is_inside_tree():
 		add_child(top_left_anchor)
+	if not bottom_right_anchor.is_inside_tree():
 		add_child(bottom_right_anchor)
 
 
 func on_texture_source_changed():
-	if Engine.is_editor_hint():
-		if not texture:
-			texture = ViewportTexture.new()
-		texture.viewport_path = EditorInterface.get_edited_scene_root().get_path_to(texture_source.get_child(0))
+	if is_instance_valid(texture_source):
+		var ptsd := texture_source.smallest_size * 0.5
+		pixel_size = 0.01
+		top_left_anchor.position.x = -ptsd.x * pixel_size
+		top_left_anchor.position.y = -ptsd.y * pixel_size
+		bottom_right_anchor.position.x = ptsd.x * pixel_size
+		bottom_right_anchor.position.y = ptsd.y * pixel_size
+		texture = (texture_source.get_child(0) as Viewport).get_texture()
 
 
 
@@ -40,7 +40,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _process(_delta: float) -> void:
-	if Engine.is_editor_hint():
+	if not is_instance_valid(texture_source):
 		return
 	var active_cam := get_viewport().get_camera_3d()
 	if not is_instance_valid(active_cam):
@@ -49,15 +49,6 @@ func _process(_delta: float) -> void:
 			.distance_to(active_cam.unproject_position(bottom_right_anchor.global_position))
 	)
 	var current_texture_diameter = Vector2.ZERO.distance_to((texture_source.get_child(current_texture_idx) as SubViewport).size)
-	$Label.text = """
-		unprojected_pos: %s
-		my diameter: %f
-		current texture diameter: %f
-	""" % [
-		str(get_viewport().get_camera_3d().unproject_position(global_position)),
-		current_sprite_diameter,
-		current_texture_diameter,
-	]
 	if current_sprite_diameter > current_texture_diameter and current_texture_idx < texture_source.get_children().size() - 1:
 		current_texture_idx += 1
 		pixel_size *= 0.5
