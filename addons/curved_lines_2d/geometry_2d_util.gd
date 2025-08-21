e76655c86dd022668bde39b2605ed7d9158af104
René van der Ark <r.van.der.ark@gmail.com>
2025-08-21 17:04:03 +0200

    greatly simplifies slice_polygon_vertical.

--- addons/curved_lines_2d/geometry_2d_util.gd
+++ addons/curved_lines_2d/geometry_2d_util.gd
@@ -22,66 +22,22 @@ static func get_polygon_center(points : PackedVector2Array) -> Vector2:
 
 
 static func slice_polygon_vertical(polygon : PackedVector2Array, slice_target : Vector2) -> Array[PackedVector2Array]:
-	var box := get_polygon_bounding_rect(polygon)
+	var box := get_polygon_bounding_rect(polygon).grow(1.0)
 	if not box.has_point(slice_target):
 		return [polygon]
-
-	var intersections : Array[PackedVector2Array] = Geometry2D.intersect_polyline_with_polygon(
-		[Vector2(slice_target.x, box.position.y), Vector2(slice_target.x, box.position.y + box.size.y)], polygon
-	)
-	if intersections.is_empty():
-		return [polygon]
-	var relevant_intersection_idx = intersections.find_custom(
-		func(inters): return (
-			(inters[0].y < slice_target.y and inters[1].y > slice_target.y) or
-			(inters[0].y > slice_target.y and inters[1].y < slice_target.y)
-		)
-	)
-	if relevant_intersection_idx == -1:
-		return [polygon]
-
-	# adaptation of knife tool:
-	# https://github.com/mrkdji/knife-tool/blob/64f5838fa79192bc0c221cf36d53f8403ee0ffc5/knife_tool.gd#L299-L349
-	var intersection = intersections[relevant_intersection_idx]
-	var does_intersect = false
-	var result : Array[PackedVector2Array] = []
-	var intersection_point_a = intersection[0]
-	var intersection_point_b = intersection[intersection.size() - 1]
-	for extreme in [intersection_point_a, intersection_point_b]:
-		for i in polygon.size():
-			var next_index = posmod(i + 1, polygon.size())
-			var point_on_segment = Geometry2D.get_closest_point_to_segment(
-				extreme,
-				polygon[i], polygon[next_index])
-			# check if the cut pass through the polygon vertices
-			if (polygon[i].distance_to(extreme) < THRESHOLD or
-				polygon[next_index].distance_to(extreme) < THRESHOLD):
-				does_intersect = true
-			elif point_on_segment.distance_to(extreme) < THRESHOLD:
-				does_intersect = true
-				polygon.insert(next_index, point_on_segment)
-				break
-
-	var intersection_point_a_index = 0
-	while polygon[intersection_point_a_index].distance_to(intersection_point_a) > THRESHOLD:
-		intersection_point_a_index += 1
-
-	for step in [1, -1]:
-		var polyslice : PackedVector2Array = []
-		var index = intersection_point_a_index
-		while polygon[index].distance_to(intersection_point_b) > THRESHOLD:
-			polyslice.append(polygon[index])
-			index = posmod(index + step, polygon.size())
-		polyslice.append(intersection_point_b)
-		var internal_points_index = intersection.size() - 2
-		while internal_points_index > 0:
-			polyslice.append(intersection[internal_points_index])
-			internal_points_index -= 1
-		result.push_back(polyslice)
-	return result
+	return Geometry2D.intersect_polygons([
+		box.position,
+		Vector2(slice_target.x, box.position.y),
+		Vector2(slice_target.x, box.position.y + box.size.y),
+		Vector2(box.position.x, box.position.y + box.size.y),
+	], polygon) + Geometry2D.intersect_polygons([
+		Vector2(slice_target.x, box.position.y),
+		Vector2(box.position.x + box.size.x, box.position.y),
+		box.position + box.size,
+		Vector2(slice_target.x, box.position.y + box.size.y),
+	], polygon)
 
 
-## TODO: document
 static func apply_polygon_bool_operation_in_place(
 		current_polygons : Array[PackedVector2Array],
 		other_polygons : Array[PackedVector2Array],
