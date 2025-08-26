37c3e24da47c33b8c8ecf10363ab7ba73de94a82
René van der Ark <r.van.der.ark@gmail.com>
2025-08-26 17:06:14 +0200

    adds polygons_updated signal fixing #147. Makes Rat's Return use it.

--- addons/curved_lines_2d/examples/rat/rats_return.gd
+++ addons/curved_lines_2d/examples/rat/rats_return.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var collision_polygon_map : Dictionary[ScalableVectorShape2D, Array] = {}
+
 func _on_drop_zone_body_entered(body: Node2D) -> void:
 	if 'die' in body:
 		body.die()
@@ -16,10 +18,9 @@ func _on_rat_place_shape(global_pos: Vector2, curve: Curve2D) -> void:
 	(new_shape.polygon.texture as NoiseTexture2D).noise = FastNoiseLite.new()
 	(new_shape.polygon.texture as NoiseTexture2D).seamless = true
 	new_shape.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
-	new_shape.collision_object = StaticBody2D.new()
 	new_shape.add_to_group("blocks")
-	new_shape.add_child(new_shape.collision_object)
 	new_shape.add_child(new_shape.polygon)
+	new_shape.polygons_updated.connect(_add_collision_polygons)
 	add_child(new_shape)
 
 
@@ -31,6 +32,21 @@ func _on_rat_cut_shapes(global_pos: Vector2, curve: Curve2D) -> void:
 	add_child(new_shape)
 	for block in get_tree().get_nodes_in_group("blocks"):
 		if Rect2(new_shape.position, new_shape.get_bounding_rect().size).intersects(
-			Rect2(block.position, block.get_bounding_rect().size)
+			Rect2(block.position, block.get_bounding_rect().size), true
 		):
 			(block as ScalableVectorShape2D).add_clip_path(new_shape)
+
+
+func _add_collision_polygons(polygons : Array[PackedVector2Array], _poly_strokes : Array[PackedVector2Array], svs : ScalableVectorShape2D):
+	if svs in collision_polygon_map:
+		for old_poly : Node in collision_polygon_map.get(svs):
+			old_poly.queue_free()
+		collision_polygon_map.get(svs).clear()
+	else:
+		collision_polygon_map[svs] = []
+	for poly in polygons:
+		var col_poly := CollisionPolygon2D.new()
+		col_poly.transform = svs.transform
+		col_poly.polygon = poly
+		collision_polygon_map[svs].append(col_poly)
+		%BlockStaticBody2D.add_child(col_poly)
