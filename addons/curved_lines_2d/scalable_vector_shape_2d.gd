c3ca0d0a61b6816451559568bff5d2eb76692747
René van der Ark <r.van.der.ark@gmail.com>
2026-06-16 10:50:30 +0200

    adds warning about rigging with active AnimationPlayer's reset track

--- addons/curved_lines_2d/scalable_vector_shape_2d.gd
+++ addons/curved_lines_2d/scalable_vector_shape_2d.gd
@@ -350,7 +350,7 @@ var stroke_width := 10.0:
 ## via the [member deformation_map] to enable deforming the shape.
 ## The entire [ScalableVectorShape2]'s position and rotation can be set via
 ## [member bone].
-## ⚠️ Make sure that any AnimationPlayer's RESET track for this shape is removed before
+## ⚠️ Make sure that any [AnimationPlayer]'s RESET track for this shape is removed before
 ## rigging your model to a new skeleton!
 @export var skeleton : Skeleton2D = null
 
