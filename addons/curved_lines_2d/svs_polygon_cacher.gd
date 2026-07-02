9e33eff6105f30c9cbe597eb61cb74ee276b1c60
René van der Ark <r.van.der.ark@gmail.com>
2026-07-02 23:15:10 +0200

    first working outline cache.

--- addons/curved_lines_2d/svs_polygon_cacher.gd
+++ addons/curved_lines_2d/svs_polygon_cacher.gd
@@ -1,13 +1,22 @@
-@tool
-extends Node
-class_name SVSPolygonCacher
+class_name SVSPolygonCacher extends Node
 
 @export var animation_player : AnimationPlayer: set = _on_animation_player_assigned
+@export var snap_resolution := 0.01
+
 
 var _caching_enabled := false
 var _caching_locked := false
-var _frame_count := 0
 var _prev_animation := ""
+
+var _outline_cache : Dictionary[String, PackedVector2Array] = {}
+
+func _ready() -> void:
+	if not get_parent() is ScalableVectorShape2D:
+		push_error("SVSPolygonCacher must be a child of a ScalableVectorShape2D.")
+		queue_free()
+		return
+	(get_parent() as ScalableVectorShape2D).cache_manager = self
+
 
 func _on_animation_player_assigned(a : AnimationPlayer) -> void:
 	if is_instance_valid(animation_player) and animation_player.current_animation_changed.is_connected(_on_animation_changed):
@@ -27,13 +36,23 @@ func _on_animation_changed(new_name : String) -> void:
 
 func set_caching_enabled() -> void:
 	_caching_enabled = true
-	_frame_count = 0
 
 
-func _process(delta: float) -> void:
-	if not _caching_enabled:
+func _mk_cache_key() -> String:
+	return str(snappedf(animation_player.current_animation_position, snap_resolution)) + ":" + animation_player.current_animation
+
+
+func has_outline() -> bool:
+	if _caching_locked or not _caching_enabled:
+		return false
+	return _mk_cache_key() in _outline_cache
+
+
+func get_outline() -> PackedVector2Array:
+	return _outline_cache[_mk_cache_key()]
+
+
+func set_outline(outline : PackedVector2Array) -> void:
+	if _caching_locked or not _caching_enabled:
 		return
-	if _caching_locked:
-		return
-	_frame_count += 1
-	print("caching active: ", animation_player.current_animation)
+	_outline_cache[_mk_cache_key()] = outline.duplicate()
