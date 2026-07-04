34b6abc1124a8dda5b1cd13e30314d95c6ead300
René van der Ark <r.van.der.ark@gmail.com>
2026-07-04 21:39:44 +0200

    adds toggle to enable and disable recording.

--- addons/curved_lines_2d/svs_polygon_cacher.gd
+++ addons/curved_lines_2d/svs_polygon_cacher.gd
@@ -1,13 +1,25 @@
 class_name SVSPolygonCacher extends Node
 
 @export var animation_player : AnimationPlayer: set = _on_animation_player_assigned
-@export var snap_resolution := 0.01
+@export var is_recording := false:
+	set(new_val):
+		is_recording = new_val
+		_cache_locks = 0
+
+
+@export var snap_resolution := 0.01:
+	set(new_snap):
+		snap_resolution = new_snap
+		is_recording = false
+		_outline_cache.clear()
 
 
 var _cache_locks : int = 0
 var _prev_animation := ""
 var _have_warned := false
 var _outline_cache : Dictionary[String, PackedVector2Array] = {}
+var cache_hits := 0
+var cache_misses := 0
 
 func _ready() -> void:
 	if not 'svs_cache_manager' in get_parent():
@@ -26,9 +38,10 @@ func _on_animation_player_assigned(a : AnimationPlayer) -> void:
 
 
 func _on_animation_changed(new_name : String) -> void:
-	if animation_player.get_blend_time(_prev_animation, new_name) > 0.0 and not _have_warned:
-		push_warning("Caching is unstable when using transitions between animations")
-		_have_warned = true
+	if animation_player.get_blend_time(_prev_animation, new_name) > 0.0:
+		if is_recording and not _have_warned:
+			push_warning("Caching is unstable when using transitions between animations")
+			_have_warned = true
 	_cache_locks += 1
 	get_tree().create_timer(animation_player.get_blend_time(_prev_animation, new_name) * 2).timeout.connect(_on_cache_lock_timer_timeout)
 	_prev_animation = new_name
@@ -49,7 +62,15 @@ func _mk_cache_key() -> String:
 func has_outline() -> bool:
 	if _cache_locks > 0:
 		return false
-	return _mk_cache_key() in _outline_cache
+	var k := _mk_cache_key()
+	if k in _outline_cache:
+		if is_recording:
+			cache_hits += 1
+		return true
+	else:
+		if is_recording:
+			cache_misses += 1
+		return false
 
 
 func get_outline() -> PackedVector2Array:
@@ -57,6 +78,8 @@ func get_outline() -> PackedVector2Array:
 
 
 func set_outline(outline : PackedVector2Array) -> void:
+	if not is_recording:
+		return
 	if _cache_locks > 0:
 		return
 	_outline_cache[_mk_cache_key()] = outline.duplicate()
