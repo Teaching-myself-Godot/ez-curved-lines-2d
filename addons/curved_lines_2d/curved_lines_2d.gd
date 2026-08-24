5ec77413e1d3fce46251eb247a961124ffc1d1b1
René van der Ark <r.van.der.ark@gmail.com>
2026-08-24 20:59:31 +0200

    Revert "extracts Geometry2DUtil.get_sliced_curve_segment"
    This reverts commit 1d447975216c602ac6e08ed6fa718792f8e57c2b.

--- addons/curved_lines_2d/curved_lines_2d.gd
+++ addons/curved_lines_2d/curved_lines_2d.gd
@@ -1933,6 +1933,20 @@ func _add_point_to_curve(svs : ScalableVectorShape2D, local_pos : Vector2,
 	undo_redo.commit_action()
 
 
+func _add_point_on_curved_segment(svs : ScalableVectorShape2D, placement_point : Vector2, before_segment : int) -> void:
+	var sliced_segment := svs.get_sliced_curve_segment(before_segment, placement_point)
+	_add_point_to_curve(svs, placement_point, Vector2.ZERO, Vector2.ZERO, before_segment, false)
+	undo_redo.add_do_method(svs.curve, "set_point_out", before_segment - 1, sliced_segment.get_point_out(0))
+	undo_redo.add_undo_method(svs.curve, "set_point_out", before_segment -1, svs.curve.get_point_out(before_segment - 1))
+	undo_redo.add_do_method(svs.curve, "set_point_in", before_segment, sliced_segment.get_point_in(1))
+	undo_redo.add_undo_method(svs.curve, "set_point_in", before_segment, svs.curve.get_point_in(before_segment))
+	undo_redo.add_do_method(svs.curve, "set_point_out", before_segment, sliced_segment.get_point_out(1))
+	undo_redo.add_undo_method(svs.curve, "set_point_out", before_segment, svs.curve.get_point_out(before_segment))
+	undo_redo.add_do_method(svs.curve, "set_point_in", before_segment + 1, sliced_segment.get_point_in(2))
+	undo_redo.add_undo_reference(svs.curve)
+	undo_redo.commit_action()
+
+
 func _create_arc(svs :  ScalableVectorShape2D, start_point_idx : int) -> void:
 	if svs.curve.point_count < 2:
 		return
@@ -2268,11 +2282,9 @@ func _apply_valid_knife_cuts(svs : ScalableVectorShape2D, cursor_pos : Vector2) 
 		var cut_start_pos := _knife_intersections.pop_front()
 		var cut_end_pos := _knife_intersections.pop_front()
 		var cutting_line := Geometry2DUtil.get_polyline_segment(_pencil_stroke, cut_start_pos, cut_end_pos)
+
 		var fitness_prep := BasicFit.prepare_polyline_segments(cutting_line, _get_basic_fit_snap(cutting_line))
 		var curve := BasicFit.fit_curve_to_polyline(cutting_line, fitness_prep)
-		Geometry2DUtil.cut_bezier_with_bezier(svs.curve, svs.curve_to_local(curve),
-				svs.max_stages, svs.tolerance_degrees)
-
 		var cut_start_segment_idx := Geometry2DUtil.find_curve_segment_idx_for_point(
 			svs.curve, svs.to_local(cut_start_pos), svs.max_stages, svs.tolerance_degrees
 		)
@@ -2282,6 +2294,8 @@ func _apply_valid_knife_cuts(svs : ScalableVectorShape2D, cursor_pos : Vector2) 
 		)
 		_add_point_on_curved_segment(svs, svs.to_local(cut_end_pos), cut_end_segment_idx + 1)
 
+		#Geometry2DUtil.cut_bezier_with_bezier(svs.curve, svs.curve_to_local(curve),
+				#svs.max_stages, svs.tolerance_degrees)
 		var cut_svs := ScalableVectorShape2D.new()
 		var rt := EditorInterface.get_edited_scene_root()
 		var ln := Line2D.new()
